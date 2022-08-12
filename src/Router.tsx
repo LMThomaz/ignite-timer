6fa5a44c8b942740f85af9a04672593cde647beb
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-08-12 08:42:52 -0300

    component Header and Layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,8 +7,8 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/history" element={<History />} />
         <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
       </Route>
     </Routes>
   )
